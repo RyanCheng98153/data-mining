5efafeec091b699b19c8fd82c42fbd187ad54d26
ryan98153 <ryan98153@gmail.com>
2025-10-06 21:22:31 +0800

    feat(hw1): replace NaN with interpolate

--- hw1/main.py
+++ hw1/main.py
@@ -26,11 +26,33 @@ def main():
             np.nan
         )
     
-    # 如果 row 中的 value_columns 全部都是 NaN，就把該 row 刪掉
     for i, row in train_df.iterrows():
-        if row.iloc[value_columns].isna().all():
+        # 找出該 row 中不是 NaN 的 value_columns 的 index
+        not_nan = row.iloc[value_columns].notna().to_numpy().nonzero()[0]
+        
+        if len(not_nan) == len(value_columns):
+            # No need to fill, all values are present
+            continue
+        
+        # 如果該 row 中只有一個 value_columns 不是 NaN，就把該 row 全部設為該 value
+        if len(not_nan) == 1:
+            train_df.iloc[i, value_columns] = np.nan  # 先設為 NaN
+            # only_value = row.iloc[value_columns].iloc[not_nan[0]]
+            # train_df.iloc[i, value_columns] = only_value
+            # print(f"Row {i} \t, {row.Date}\t, {row.ItemName.strip()}\t only has one value_column, set all value_columns to {only_value}")
+            continue
+        
+        # 如果 row 中的 value_columns 全部都是 NaN，就把該 row 刪掉
+        if len(not_nan) == 0:
             train_df.drop(i, inplace=True)
-            print(f"Drop row {i} because all value_columns are NaN")
+            # print(f"Drop row {i}\t, {row.Date}\t, {row.ItemName.strip()}\t because all value_columns are NaN")
+            continue
+        
+        # 如果該 row 中有多個 value_columns 不是 NaN，就用線性插值法 (linear interpolation) 補值
+        np_row = row.iloc[value_columns].to_numpy(dtype=float)
+        x = np.arange(len(np_row))
+        train_df.iloc[i, value_columns] = np.interp(x, x[not_nan], np_row[not_nan])
     
+        
 if __name__ == "__main__":
     main()
